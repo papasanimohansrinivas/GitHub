37bbf2df27f170bb6fd390c35d8b82e83f0395ad
papasani\mohan/srinivas <papasani.mohansrinivas@gmail.com>
2016-10-25 19:34:29 +0530

    Add files via upload

--- StringADT.java
+++ StringADT.java
@@ -1,85 +1,109 @@
-import java.util.*;
-
 class StringADT{
-	char[] arr;
-	char [] ans;
-	public StringADT(char[] initial){
-		arr=initial;
+	char array[];
+	public StringADT(char[] arr){
+		array=arr;
 	}
-	public char[] substring(int start, int end){
+	public int indexOf(char element){
 
-		char j[] = new char[10];
-		int length=arr.length;
-
-		if(start>0 && start<length && 0<end && end<length && end>start){
-			char[] h = new char[end-start];
-			for (int y=start,q=0;y<end&&q<length ;y++,q++ ) {
-				h[q]=arr[y];
+		for (int g=0;g<array.length ;g++ ) {
+			if (array[g]==element) {
+				return g;
 			}
-			j=h;
-			System.out.println();
 		}
-		if (start<0 || start>length || end<0 || end>length || end<start ) {
-			throw new IndexOutOfBoundsException("Index Out of Bounds Exception");
-		}
-		
-		return j;
-		//j=h;
 
-		
-
-		//return j;
-		
-				
-		
+		return -1;
 
 	}
-	public boolean equals(char[] par){
-		boolean result=false;
+	public int length(){
+
+		if(array.length==0){
+			return 0;
+
+		}
+		else {
+			return array.length;
+		}
+
+	
+	}
+	public int count(char[] arr1){
+		int count1=0;
+		String q ="";
+		String s1="";
+		for (int y=0;y<array.length ;y++ ) {
+			q=q+array[y];
+		}
+		for (int x=0;x<arr1.length ;x++ ) {
+			s1=s1+arr1[x];
+		}
+		if (array.length<arr1.length) {
+
+			return 0;
+			
+		}/* if given array contains 1 character */
 		
-		String temp1="";
-		
-		if (par.length==arr.length) {
-			int count=0;
-			for (int q=0;q<arr.length ;q++ ) {
-				if (arr[q]==par[q]) {
-					count++;
+		else if(arr1.length==1){
+
+			for (int v=0;v<arr1.length ;v++ ) {
+				for (int h=0;h<array.length ;h++ ) {
+					if (array[h]==arr1[v]) {
+						count1++;
+					}
 				}
 			}
-			if (count==arr.length) {
-				result =true;
+			return count1;
+
+		}/* if given array */
+		else if(array.length>arr1.length){
+			
+			boolean w =false;
+
+			try{
+				for (int l=0;l<array.length ;l++ ) {
+					if(arr1[0]==array[l]){
+						if (contains(l,arr1)==true) {
+							count1++;
+						}
+					}
+				}
+			}
+			catch (IndexOutOfBoundsException e) {
+				System.out.println(e.getMessage());
+				System.out.println();
+			}
+		}
+		else if (q.equals(s1)) {
+			return 1;
+		}
+
+		return count1;
+	}
+	public boolean contains(int index,char[] arr1){
+		int count=0;
+		for (int e=index,u=0;e<array.length&&u<arr1.length ;e++,u++ ) {
+			if (array[e]==arr1[u]) {
+				count++;
 				
 			}
-			else{
-				result =false;
-			}
+		}
+		if (count==arr1.length) {
+			return true;
 		}
 		else{
-			result=false;
+			return false;
 		}
-		return result;
-		
-		
+
 	}
-	public char[] replace(char[] given , char[] replace){
-		String m="";
-		
 
-		//System.out.println(e);
-
-
-		return arr;
-	}
-	
 	public static void main(String[] args) {
-		char r[] = {'M','S','I','T','E','x','a','m','i','t','e'};
-		char given[] = {'i','t','e'};
-		char replace[] ={'i','e','t','e'};
+		char testCase[] = new char[]{'m','i','s','s','i','s','s','i','p','p','i'};
+		StringADT h = new StringADT(testCase);
+		System.out.println(h.indexOf('i'));
+		testCase = new char[]{'i','s'};
+		System.out.println(h.count(testCase));
 
-		StringADT to = new StringADT(r);
 
-		to.replace(given,replace); 
 
-		
+
 	}
 }
